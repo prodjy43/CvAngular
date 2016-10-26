6b5b7dadab23022f7a8449950292169b2b2c9c5f
prodjy43 <prodjy43@gmail.com>
2016-10-26 11:25:09 +0200

    smooth scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,5 +89,20 @@ setTimeout(function(){
     master.commit({
         message: "Fetching next story...",
         sha1: "Coming soon - "
-    })
-},1000);
+    });
+
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top - 75
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+},2000);
